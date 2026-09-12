51ab676a670c191574bc4fc466e3ec856290ecf4
Lohith <lohivadivel@gmail.com>
2026-09-12 11:53:21 +0530

    First Code

--- amstrong.py
+++ amstrong.py
@@ -1,1 +1,18 @@
-Lohi
+# Prime Number
+
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print(num, "is not a Prime number")
+else:
+    is_prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(num, "is a Prime number")
+    else:
+        print(num, "is not a Prime number")
